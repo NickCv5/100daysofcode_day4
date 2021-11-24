c7525f7fb352a0a831bbd2e75f0e1287cb0abdad
Nick <nickcv4@gmail.com>
2021-11-24 08:43:51 -0500

    Moved images to list

--- main.py
+++ main.py
@@ -27,31 +27,27 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
+game_images = [ rock, paper, scissors]
 choice_int = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.\n"))
 
 if choice_int == 0:
     choice = "rock"
-    print(rock)
 elif choice_int == 1:
     choice = "paper"
-    print(paper)
 elif choice_int == 2:
     choice = "scissors"
-    print(scissors)
+
+print(game_images[choice_int])
 
 if random_int == 0:
     computer_choice = "rock"
-    graphic = rock
 elif random_int == 1:
     computer_choice = "paper"
-    graphic = paper
 else:
     computer_choice = "scissors"
-    graphic = scissors
 
 print(f"Computer chose:")
-print(graphic)
+print(game_images[random_int])
 
 if computer_choice == choice:
     print(f"It's a tie!")
